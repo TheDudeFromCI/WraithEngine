f409bdf0f5053f4a9818bdca44e9661c092d1dc0
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-03-23 16:46:27 -0700

    Added spread sheet alignment improvements.

--- WraithEngine/src/wraith/library/WindowUtil/SpreadSheet.java
+++ WraithEngine/src/wraith/library/WindowUtil/SpreadSheet.java
@@ -3,6 +3,7 @@ package wraith.library.WindowUtil;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
@@ -11,12 +12,14 @@ import javax.swing.JPanel;
 public class SpreadSheet extends JPanel{
 	private String[] columnNames;
 	private double[] columnSizes;
+	private byte[] alignments;
 	private int rowHeight;
 	private final ArrayList<Object[]> data = new ArrayList<>();
 	private static final Color ALT_COLOR = new Color(230, 255, 230);
-	public SpreadSheet(String[] columnNames, double[] columnSizes, int rowHeight){
+	public SpreadSheet(String[] columnNames, double[] columnSizes, byte[] alignments, int rowHeight){
 		this.columnNames=columnNames;
 		this.columnSizes=columnSizes;
+		this.alignments=alignments;
 		this.rowHeight=rowHeight;
 	}
 	public void addRow(Object[] row){
@@ -34,6 +37,8 @@ public class SpreadSheet extends JPanel{
 		int width = getWidth();
 		int height = getHeight();
 		int rowsShown = height/rowHeight;
+		FontMetrics fm = g.getFontMetrics();
+		int textY = (int)(fm.getAscent()+(rowHeight-(fm.getAscent()+fm.getDescent()))*0.5);
 		for(int i = 0; i<=rowsShown; i++){
 			g.setColor(i==0?Color.lightGray:i%2==1?Color.white:ALT_COLOR);
 			g.fillRect(0, rowHeight*i, width, rowHeight);
@@ -42,9 +47,17 @@ public class SpreadSheet extends JPanel{
 			if(i==0){
 				Font oldFont = getFont();
 				g.setFont(new Font(oldFont.getName(), Font.BOLD, oldFont.getSize()));
-				for(int j = 0; j<columnNames.length; j++)g.drawString(columnNames[j], (int)(width*columnSizes[j])+3, rowHeight*(i+1)-3);
+				for(int j = 0; j<columnNames.length; j++)g.drawString(columnNames[j], (int)(((((j==columnSizes.length-1?1:columnSizes[j+1])-columnSizes[j])*width)-fm.stringWidth(columnNames[j].toString()))*0.5+width*columnSizes[j]), rowHeight*i+textY);
 				g.setFont(oldFont);
-			}else if(i-1<data.size())for(int j = 0; j<columnSizes.length; j++)g.drawString(data.get(i-1)[j].toString(), (int)(width*columnSizes[j])+3, rowHeight*(i+1)-3);
+			}else{
+				if(i-1<data.size()){
+					for(int j = 0; j<columnSizes.length; j++){
+						if(alignments[j]==-1)g.drawString(data.get(i-1)[j].toString(), (int)(width*columnSizes[j])+3, rowHeight*i+textY);
+						else if(alignments[j]==0)g.drawString(data.get(i-1)[j].toString(), (int)(((((j==columnSizes.length-1?1:columnSizes[j+1])-columnSizes[j])*width)-fm.stringWidth(data.get(i-1)[j].toString()))*0.5+width*columnSizes[j]), rowHeight*i+textY);
+						else g.drawString(data.get(i-1)[j].toString(), (int)(width*columnSizes[j])+3, rowHeight*i+textY);
+					}
+				}
+			}
 		}
 		g.setColor(Color.black);
 		for(int i = 0; i<columnSizes.length-1; i++)g.drawLine((int)(width*columnSizes[i+1]), 0, (int)(width*columnSizes[i+1]), height);
@@ -54,5 +67,16 @@ public class SpreadSheet extends JPanel{
 		data.clear();
 		data.addAll(rows);
 		repaint();
+	}
+	public void setNames(String[] columnNames){
+		this.columnNames=columnNames;
+		repaint();
+	}
+	public void updateColumnData(String[] columnNames, double[] columnSizes, byte[] alignments, int rowHeight){
+		this.columnNames=columnNames;
+		this.columnSizes=columnSizes;
+		this.rowHeight=rowHeight;
+		this.alignments=alignments;
+		data.clear();
 	}
 }
