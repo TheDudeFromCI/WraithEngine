76388b1b74c8499ee8d87b02d22175aa486c0599
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-07 16:26:54 -0700

    Fixed bug with tile placement.

--- WraithEngine/src/wraith/library/MiscUtil/ImageUtil.java
+++ WraithEngine/src/wraith/library/MiscUtil/ImageUtil.java
@@ -7,9 +7,9 @@ public class ImageUtil{
 		if(img==null)throw new IllegalArgumentException("Image cannot be null!");
 		if(rows<1||cols<1)throw new IllegalArgumentException("Rows and columns must be at least 1!");
 		BufferedImage[] bufs = new BufferedImage[rows*cols];
-		float width = img.getWidth()/(float)cols;
-		float height = img.getHeight()/(float)rows;
-		for(int i = 0; i<bufs.length; i++)bufs[i]=img.getSubimage((int)(i/cols*width), (int)(i%cols*height), (int)width, (int)height);
+		int width = img.getWidth()/cols;
+		int height = img.getHeight()/rows;
+		for(int i = 0; i<bufs.length; i++)bufs[i]=img.getSubimage(i%cols*width, i/cols*height, width, height);
 		return bufs;
 	}
 }
