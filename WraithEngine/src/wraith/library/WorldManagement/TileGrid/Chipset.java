7197b052b27f63de3258eacf86becf3274f2fbf2
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-07 16:26:23 -0700

    Added chipset helper class.

---
+++ WraithEngine/src/wraith/library/WorldManagement/TileGrid/Chipset.java
@@ -0,0 +1,26 @@
+package wraith.library.WorldManagement.TileGrid;
+
+import java.awt.image.BufferedImage;
+import wraith.library.MiscUtil.ImageUtil;
+
+public class Chipset{
+	private final int rows, cols;
+	private final BufferedImage[] tiles;
+	public Chipset(BufferedImage img, int rows, int cols){
+		if(img==null)throw new IllegalArgumentException("Image cannot be null!");
+		if(rows<1||cols<1)throw new IllegalArgumentException("Rows and cols must be at least 1!");
+		tiles=ImageUtil.splitImage(img, rows, cols);
+		this.cols=cols;
+		this.rows=rows;
+	}
+	public Chipset(BufferedImage img, int size){
+		if(img==null)throw new IllegalArgumentException("Image cannot be null!");
+		if(size<1)throw new IllegalArgumentException("Size cannot be less then 1!");
+		this.cols=img.getWidth()/size;
+		this.rows=img.getHeight()/size;
+		tiles=ImageUtil.splitImage(img, rows, cols);
+	}
+	public BufferedImage getTile(int x, int y){ return tiles[y*cols+x]; }
+	public int getRows(){ return rows; }
+	public int getCols(){ return cols; }
+}
