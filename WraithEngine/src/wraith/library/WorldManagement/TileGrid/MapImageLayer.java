15b828e8c7c875af5863b59b9a180f46be0009fd
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-07 07:08:27 -0700

    Made a slight adjustment, to make the system more flexible. It now accepts tile graphics of any size, and scales them.

--- WraithEngine/src/wraith/library/WorldManagement/TileGrid/MapImageLayer.java
+++ WraithEngine/src/wraith/library/WorldManagement/TileGrid/MapImageLayer.java
@@ -20,20 +20,28 @@ public class MapImageLayer{
 		animatedTiles.clear();
 		Tile tile;
 		TileMaterial tileMaterial;
+		BufferedImage img;
 		for(int x = 0; x<map.getSizeX(); x++){
 			for(int z = 0; z<map.getSizeZ(); z++){
 				tile=map.getTileAt(x, y, z);
 				if(tile==null)continue;
 				tileMaterial=tile.getMaterial();
 				if(tileMaterial.isAnimated())animatedTiles.add(tile);
-				else g.drawImage(tileMaterial.getImage(), x*map.getCameraScale(), z*map.getCameraScale(), (x+1)*map.getCameraScale(), (z+1)*map.getCameraScale(), 0, 0, 16, 16, null);
+				else{
+					img=tileMaterial.getImage();
+					g.drawImage(img, x*map.getCameraScale(), z*map.getCameraScale(), (x+1)*map.getCameraScale(), (z+1)*map.getCameraScale(), 0, 0, img.getWidth(), img.getHeight(), null);
+				}
 			}
 		}
 		g.dispose();
 	}
 	public void render(Graphics2D g){
 		g.drawImage(staticImage, -map.getCameraX(), -map.getCameraZ(), null);
-		for(Tile t : animatedTiles)g.drawImage(t.getMaterial().getImage(), t.getX()*map.getCameraScale()-map.getCameraX(), t.getZ()*map.getCameraScale()-map.getCameraZ(), (t.getX()+1)*map.getCameraScale()-map.getCameraX(), (t.getZ()+1)*map.getCameraScale()-map.getCameraZ(), 0, 0, 16, 16, null);
+		BufferedImage img;
+		for(Tile t : animatedTiles){
+			img=t.getMaterial().getImage();
+			g.drawImage(img, t.getX()*map.getCameraScale()-map.getCameraX(), t.getZ()*map.getCameraScale()-map.getCameraZ(), (t.getX()+1)*map.getCameraScale()-map.getCameraX(), (t.getZ()+1)*map.getCameraScale()-map.getCameraZ(), 0, 0, img.getWidth(), img.getHeight(), null);
+		}
 	}
 	public ArrayList<Tile> getAnimatedTiles(){ return animatedTiles; }
 }
