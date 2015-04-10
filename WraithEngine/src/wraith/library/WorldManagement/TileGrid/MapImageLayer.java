953874c70ad252e1488b5b69093ee68d3e8ceab1
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-10 02:54:08 -0700

    Added a simple shortcut to ignore tiles outside of possible render range.

--- WraithEngine/src/wraith/library/WorldManagement/TileGrid/MapImageLayer.java
+++ WraithEngine/src/wraith/library/WorldManagement/TileGrid/MapImageLayer.java
@@ -15,6 +15,7 @@ public class MapImageLayer{
 		repaint();
 	}
 	public void repaint(){
+		long time = System.currentTimeMillis();
 		if(staticImage==null||staticImage.getWidth()!=map.getCameraWidth()||staticImage.getHeight()!=map.getCameraHeight()){
 			if(g!=null)g.dispose();
 			staticImage=new BufferedImage(map.getCameraWidth(), map.getCameraHeight(), BufferedImage.TYPE_INT_ARGB);
@@ -22,24 +23,22 @@ public class MapImageLayer{
 			g.setBackground(new Color(0, 0, 0, 0));
 		}else g.clearRect(0, 0, staticImage.getWidth(), staticImage.getHeight());
 		Tile tile;
-		TileMaterial tileMaterial;
-		int tileX, tileZ;
+		int tileX;
 		int imageWidth = staticImage.getWidth();
 		int imageHeight = staticImage.getHeight();
 		int lowX = Math.max(map.getCameraX()/map.getCameraScale(), 0);
 		int lowZ = Math.max(map.getCameraZ()/map.getCameraScale(), 0);
-		int highX = Math.min(lowX+imageWidth/map.getCameraScale(), map.getSizeX()-1)+1;
+		int highX = Math.min(lowX+imageWidth/map.getCameraScale()+1, map.getSizeX()-1);
 		int highZ = Math.min(lowZ+imageHeight/map.getCameraScale(), map.getSizeZ()-1);
 		for(int x = lowX; x<=highX; x++){
 			tileX=x*map.getCameraScale()-map.getCameraX();
 			for(int z = lowZ; z<=highZ; z++){
 				tile=map.getTileAt(x, y, z);
 				if(tile==null)continue;
-				tileZ=z*map.getCameraScale()-map.getCameraZ();
-				tileMaterial=tile.getMaterial();
-				g.drawImage(tileMaterial.getImage(), tileX, tileZ, map.getCameraScale(), map.getCameraScale(), null);
+				g.drawImage(tile.getMaterial().getImage(), tileX, z*map.getCameraScale()-map.getCameraZ(), map.getCameraScale(), map.getCameraScale(), null);
 			}
 		}
+		System.out.println(System.currentTimeMillis()-time);
 	}
 	public void render(Graphics2D g){ g.drawImage(staticImage, 0, 0, null); }
 }
