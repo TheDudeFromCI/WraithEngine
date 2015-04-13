839fb1d7df2b77246e80744af572b9b6e33a74e9
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-13 15:59:05 -0700

    Now allows for adaptive render size, to avoid image distortion.

--- WraithEngine/src/wraith/library/WindowUtil/GameRenderer.java
+++ WraithEngine/src/wraith/library/WindowUtil/GameRenderer.java
@@ -3,5 +3,5 @@ package wraith.library.WindowUtil;
 import java.awt.Graphics2D;
 
 public interface GameRenderer{
-	public void render(Graphics2D g, int width, int height);
+	public void render(Graphics2D g, int x, int y, int width, int height);
 }
--- WraithEngine/src/wraith/library/WindowUtil/GameScreen.java
+++ WraithEngine/src/wraith/library/WindowUtil/GameScreen.java
@@ -1,20 +1,30 @@
 package wraith.library.WindowUtil;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JFrame;
-import javax.swing.Timer;
 
 public class GameScreen{
+	private int x, y, nx, ny;
+	private Color backgroundColor = Color.black;
+	private final Dimension renderSize;
+	private final Dimension screenSize;
 	public GameScreen(String name, final GameRenderer renderer, UserInputAdapter adapter){
-		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenSize=Toolkit.getDefaultToolkit().getScreenSize();
+		renderSize=new Dimension(screenSize);
 		final JFrame frame = new JFrame(){
 			@Override public void paint(Graphics g){
-				renderer.render((Graphics2D)g, screenSize.width, screenSize.height);
+				g.setColor(backgroundColor);
+				g.fillRect(0, 0, x, screenSize.height);
+				g.fillRect(x, 0, renderSize.width, y);
+				g.fillRect(x, ny, renderSize.width, y);
+				g.fillRect(nx, 0, x, screenSize.height);
+				renderer.render((Graphics2D)g, x, y, renderSize.width, renderSize.height);
 				g.dispose();
 			}
 		};
@@ -29,10 +39,28 @@ public class GameScreen{
 		frame.addMouseWheelListener(adapter);
 		frame.addKeyListener(adapter);
 		frame.setVisible(true);
-		new Timer(20, new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				frame.repaint();
-			}
-		}).start();
+		new Timer().scheduleAtFixedRate(new TimerTask(){
+			public void run(){ frame.repaint(); }
+		}, 20, 20);
 	}
+	public void setRenderSize(int width, int height, boolean scale){
+		if(scale){
+			if(Math.abs(screenSize.width-width)>Math.abs(screenSize.height-height)){
+				double scaleSize = screenSize.height/(double)height;
+				height=screenSize.height;
+				width*=scaleSize;
+			}else{
+				double scaleSize = screenSize.width/(double)width;
+				width=screenSize.width;
+				height*=scaleSize;
+			}
+		}
+		renderSize.width=width;
+		renderSize.height=height;
+		x=(screenSize.width-width)/2;
+		y=(screenSize.height-height)/2;
+		nx=x+renderSize.width;
+		ny=y+renderSize.height;
+	}
+	public void setBackgroundColor(Color color){ backgroundColor=color; }
 }
