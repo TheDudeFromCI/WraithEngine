30bb21da8d9e22ea056b8b98863e381659082cf9
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-14 13:14:01 -0700

    Now primative arrays are allowed.

--- WraithEngine/src/wraith/library/MiscUtil/StringUtil.java
+++ WraithEngine/src/wraith/library/MiscUtil/StringUtil.java
@@ -39,13 +39,97 @@ public class StringUtil{
 		wrappedLine.append(str.substring(offset));
 		return wrappedLine.toString();
 	}
-	public static String arrayToString(Object[] o){
+	public static String arrayToString(Object array){
+		if(array instanceof byte[])return arrayToString((byte[])array);
+		if(array instanceof short[])return arrayToString((short[])array);
+		if(array instanceof int[])return arrayToString((int[])array);
+		if(array instanceof long[])return arrayToString((long[])array);
+		if(array instanceof float[])return arrayToString((float[])array);
+		if(array instanceof double[])return arrayToString((double[])array);
+		if(array instanceof boolean[])return arrayToString((boolean[])array);
+		if(array instanceof char[])return arrayToString((char[])array);
+		return arrayToString((Object[])array);
+	}
+	private static String arrayToString(Object[] o){
 		StringBuilder sb = new StringBuilder();
 		String comma = ", ";
 		String empty = "";
 		sb.append("[");
-		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(o[i].toString());
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
 		sb.append("]");
 		return sb.toString();
 	}
+	private static String arrayToString(byte[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(short[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(int[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(long[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(float[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(double[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(boolean[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+	private static String arrayToString(char[] o){
+		StringBuilder sb = new StringBuilder();
+		String comma = ", ";
+		String empty = "";
+		sb.append("[");
+		for(int i = 0; i<o.length; i++)sb.append(i>0?comma:empty).append(String.valueOf(o[i]));
+		sb.append("]");
+		return sb.toString();
+	}
+
 }
