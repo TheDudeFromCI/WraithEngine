7242aace6572b339e46e9ea61061150383404494
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-14 13:44:53 -0700

    Fixed linear interpolation.

--- WraithEngine/src/wraith/library/RandomGeneration/LinearInterpolation.java
+++ WraithEngine/src/wraith/library/RandomGeneration/LinearInterpolation.java
@@ -1,5 +1,5 @@
 package wraith.library.RandomGeneration;
 
 public class LinearInterpolation implements InterpolationFunction{
-	public float interpolate(float a, float b, float frac){ return (a-b)*frac+b; }
+	public float interpolate(float a, float b, float frac){ return (1-frac)*a+frac*b; }
 }
