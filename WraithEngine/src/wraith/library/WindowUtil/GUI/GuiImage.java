877ac0d2fb06ff5f0128660069e2260e2f971ffc
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-18 10:47:39 -0700

    Created a gui image component.

---
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiImage.java
@@ -0,0 +1,43 @@
+package wraith.library.WindowUtil.GUI;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+public class GuiImage extends GuiComponent{
+	private final Dimension size = new Dimension();
+	private final Point location = new Point();
+	private final BufferedImage buf;
+	private final boolean stretch;
+	public GuiImage(BufferedImage buf, boolean stretch){
+		this.buf=buf;
+		this.stretch=stretch;
+		calculateAdjustments();
+	}
+	private void calculateAdjustments(){
+		if(stretch){
+			size.width=width;
+			size.height=height;
+			location.x=x;
+			location.y=y;
+		}else{
+			if(Math.abs(width-buf.getWidth())>Math.abs(height-buf.getHeight())){
+				double scaleSize = height/(double)buf.getHeight();
+				size.height=height;
+				size.width*=scaleSize;
+			}else{
+				double scaleSize = width/(double)buf.getWidth();
+				size.width=width;
+				size.height*=scaleSize;
+			}
+			location.x=(width-size.width)/2;
+			location.y=(height-size.height)/2;
+		}
+	}
+	@Override public void setSizeAndLocation(int x, int y, int width, int height){
+		super.setSizeAndLocation(x, y, width, height);
+		calculateAdjustments();
+	}
+	public void render(Graphics2D g){ g.drawImage(buf, location.x, location.y, size.width, size.height, null); }
+}
