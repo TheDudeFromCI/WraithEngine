d53e13618b91bc28d7ee42c045d44e9572da69a7
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-18 09:58:37 -0700

    Container now validates properly.

--- WraithEngine/src/wraith/library/WindowUtil/GUI/GuiComponent.java
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiComponent.java
@@ -13,6 +13,12 @@ public abstract class GuiComponent{
 		this.width=width;
 		this.height=height;
 	}
+	public void setSizeAndLocation(int x, int y, int width, int height){
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+	}
 	public int getX(){ return x; }
 	public int getY(){ return y; }
 	public int getWidth(){ return width; }
