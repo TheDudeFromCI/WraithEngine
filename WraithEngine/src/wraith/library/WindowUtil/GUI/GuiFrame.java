9b834736bd86ae51cdcd5e3220ab57bdffd016ba
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-19 09:22:54 -0700

    Removed unnessicry code.

--- WraithEngine/src/wraith/library/WindowUtil/GUI/GuiFrame.java
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiFrame.java
@@ -1,32 +1,11 @@
 package wraith.library.WindowUtil.GUI;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import wraith.library.MiscUtil.ImageUtil;
 
 public class GuiFrame extends GuiContainer{
-	private BufferedImage staticImage;
-	private Graphics2D g;
 	public GuiFrame(int bufferWidth, int bufferHeight){
-		super(null);
-		staticImage=ImageUtil.getBestFormat(bufferWidth, bufferHeight);
-		g=staticImage.createGraphics();
-		g.setBackground(new Color(0, 0, 0, 0));
+		super(null, bufferWidth, bufferHeight);
 		setSizeAndLocation(0, 0, bufferWidth, bufferHeight);
 	}
-	public void update(){
-		if(!needsRepaint)return;
-		g.clearRect(0, 0, width, height);
-		for(int i = 0; i<components.size(); i++)components.get(i).render(g);
-		setRepainted();
-	}
-	@Override public void dispose(){
-		super.dispose();
-		g.dispose();
-		g=null;
-		staticImage=null;
-	}
 	public void render(Graphics2D g){}
-	public BufferedImage getStaticImage(){ return staticImage; }
 }
