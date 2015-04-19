4593e13f6650f0fa05e02e882d951d0341230ac8
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-19 10:36:09 -0700

    Added gui button.

---
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiButton.java
@@ -0,0 +1,17 @@
+package wraith.library.WindowUtil.GUI;
+
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+public class GuiButton extends GuiImage{
+	private Runnable onClick;
+	public GuiButton(GuiContainer parent, BufferedImage buf, boolean stretch, Runnable onClick){
+		super(parent, buf, stretch);
+		this.onClick=onClick;
+	}
+	@Override public void dispose(){
+		super.dispose();
+		onClick=null;
+	}
+	@Override public void mouseClicked(MouseEvent e){ if(isWithinBounds(e.getPoint()))onClick.run(); }
+}
