35f005e05a19ef06431a5203fbdff1bf1822400c
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-19 10:40:01 -0700

    Panel now actually renders child components.

--- WraithEngine/src/wraith/library/WindowUtil/GUI/GuiPanel.java
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiPanel.java
@@ -3,6 +3,13 @@ package wraith.library.WindowUtil.GUI;
 import java.awt.Graphics2D;
 
 public class GuiPanel extends GuiContainer{
+	public void render(Graphics2D g){
+		GuiComponent c;
+		for(int i = 0; i<components.size(); i++){
+			c=components.get(i);
+			c.update();
+			g.drawImage(c.getPane(), c.x, c.y, c.width, c.height, null);
+		}
+	}
 	public GuiPanel(GuiContainer parent, int bufferWidth, int bufferHeight){ super(parent, bufferWidth, bufferHeight); }
-	public void render(Graphics2D g){}
 }
