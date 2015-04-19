c52e47f064606a755b47c7290ff606564827dbb7
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-19 09:48:25 -0700

    Created simple panel helper class.

--- WraithEngine/src/wraith/library/WindowUtil/GUI/GuiFrame.java
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiFrame.java
@@ -1,11 +1,8 @@
 package wraith.library.WindowUtil.GUI;
 
-import java.awt.Graphics2D;
-
-public class GuiFrame extends GuiContainer{
+public class GuiFrame extends GuiPanel{
 	public GuiFrame(int bufferWidth, int bufferHeight){
 		super(null, bufferWidth, bufferHeight);
 		setSizeAndLocation(0, 0, bufferWidth, bufferHeight);
 	}
-	public void render(Graphics2D g){}
 }
---
+++ WraithEngine/src/wraith/library/WindowUtil/GUI/GuiPanel.java
@@ -0,0 +1,8 @@
+package wraith.library.WindowUtil.GUI;
+
+import java.awt.Graphics2D;
+
+public class GuiPanel extends GuiContainer{
+	public GuiPanel(GuiContainer parent, int bufferWidth, int bufferHeight){ super(parent, bufferWidth, bufferHeight); }
+	public void render(Graphics2D g){}
+}
