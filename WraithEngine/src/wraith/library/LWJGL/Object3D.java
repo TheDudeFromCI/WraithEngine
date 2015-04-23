03e3699c79ba1eed43e379c9fedc8cfc64ded569
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-23 11:33:42 -0700

    Added 3d object classes, and camera.

---
+++ WraithEngine/src/wraith/library/LWJGL/Object3D.java
@@ -0,0 +1,5 @@
+package wraith.library.LWJGL;
+
+public abstract class Object3D{
+	public float x, y, z, pitch, yaw, roll, xSize, ySize, zSize;
+}
---
+++ WraithEngine/src/wraith/library/LWJGL/RenderableObject3D.java
@@ -0,0 +1,5 @@
+package wraith.library.LWJGL;
+
+public abstract class RenderableObject3D extends Object3D{
+	public abstract void renderObject();
+}
