73e12f2b9b480acbe5bd288d8b552acc1f6c9cfd
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-24 11:52:12 -0700

    Combined camera into world renderer.

--- WraithEngine/src/wraith/library/LWJGL/WorldRenderer.java
+++ WraithEngine/src/wraith/library/LWJGL/WorldRenderer.java
@@ -3,13 +3,16 @@ package wraith.library.LWJGL;
 import org.lwjgl.opengl.GL11;
 
 public class WorldRenderer extends RenderGroup{
+	public final Camera cam;
+	public void render(float delta){
+		update(delta);
+		cam.translateInvertMatrix();
+		draw();
+	}
+	public WorldRenderer(float fov, float aspect, float near, float far){ cam=new Camera(fov, aspect, near, far); }
 	public static void preLoop(){
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glEnable(GL11.GL_CULL_FACE);
 		GL11.glCullFace(GL11.GL_BACK);
-	}
-	public void render(float delta){
-		update(delta);
-		draw();
 	}
 }
