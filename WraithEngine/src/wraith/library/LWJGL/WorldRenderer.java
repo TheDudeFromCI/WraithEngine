596796fd5f33389956f694fe36f917ee7c012a4b
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-24 11:53:26 -0700

    Moved camera initalization due to threading issues.

--- WraithEngine/src/wraith/library/LWJGL/WorldRenderer.java
+++ WraithEngine/src/wraith/library/LWJGL/WorldRenderer.java
@@ -3,16 +3,16 @@ package wraith.library.LWJGL;
 import org.lwjgl.opengl.GL11;
 
 public class WorldRenderer extends RenderGroup{
-	public final Camera cam;
+	public Camera cam;
 	public void render(float delta){
 		update(delta);
 		cam.translateInvertMatrix();
 		draw();
 	}
-	public WorldRenderer(float fov, float aspect, float near, float far){ cam=new Camera(fov, aspect, near, far); }
-	public static void preLoop(){
+	public void preLoop(float fov, float aspect, float near, float far){
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glEnable(GL11.GL_CULL_FACE);
 		GL11.glCullFace(GL11.GL_BACK);
+		cam=new Camera(fov, aspect, near, far);
 	}
 }
