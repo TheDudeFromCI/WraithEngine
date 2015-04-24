4320485fecd12462a3aff0b3b87535ab6cc6a379
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-04-24 10:39:57 -0700

    Matrix transforms are now a lower level, so renderables can ignore them.

--- WraithEngine/src/wraith/library/LWJGL/Cube.java
+++ WraithEngine/src/wraith/library/LWJGL/Cube.java
@@ -3,41 +3,51 @@ package wraith.library.LWJGL;
 import org.lwjgl.opengl.GL11;
 
 public class Cube extends RenderableObject3D{
-	public void renderObject(){
-		GL11.glPushMatrix();
-		translateMatrix();
+	public boolean renderXUp, renderXDown, renderYUp, renderYDown, renderZUp, renderZDown;
+	protected void draw(){
 		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glColor3f(0.5f, 0.5f, 0.0f);
-		GL11.glVertex3f(0.5f, 0.5f, -0.5f);
-		GL11.glVertex3f(-0.5f, 0.5f, -0.5f);
-		GL11.glVertex3f(-0.5f, 0.5f, 0.5f);
-		GL11.glVertex3f(0.5f, 0.5f, 0.5f);
-		GL11.glColor3f(0.5f, 0.5f, 0.0f);
-		GL11.glVertex3f(0.5f, -0.5f, 0.5f);
-		GL11.glVertex3f(-0.5f, -0.5f, 0.5f);
-		GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
-		GL11.glVertex3f(0.5f, -0.5f, -0.5f);
-		GL11.glColor3f(0.5f, 0.0f, 0.0f);
-		GL11.glVertex3f(0.5f, 0.5f, 0.5f);
-		GL11.glVertex3f(-0.5f, 0.5f, 0.5f);
-		GL11.glVertex3f(-0.5f, -0.5f, 0.5f);
-		GL11.glVertex3f(0.5f, -0.5f, 0.5f);
-		GL11.glColor3f(0.5f, 0.5f, 0.0f);
-		GL11.glVertex3f(0.5f, -0.5f, -0.5f);
-		GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
-		GL11.glVertex3f(-0.5f, 0.5f, -0.5f);
-		GL11.glVertex3f(0.5f, 0.5f, -0.5f);
-		GL11.glColor3f(0.0f, 0.0f, 0.5f);
-		GL11.glVertex3f(-0.5f, 0.5f, 0.5f);
-		GL11.glVertex3f(-0.5f, 0.5f, -0.5f);
-		GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
-		GL11.glVertex3f(-0.5f, -0.5f, 0.5f);
-		GL11.glColor3f(0.5f, 0.0f, 0.5f);
-		GL11.glVertex3f(0.5f, 0.5f, -0.5f);
-		GL11.glVertex3f(0.5f, 0.5f, 0.5f);
-		GL11.glVertex3f(0.5f, -0.5f, 0.5f);
-		GL11.glVertex3f(0.5f, -0.5f, -0.5f);
+		if(renderXUp){
+			GL11.glColor3f(0.5f, 0.0f, 0.5f);
+			GL11.glVertex3f(0.5f, 0.5f, -0.5f);
+			GL11.glVertex3f(0.5f, 0.5f, 0.5f);
+			GL11.glVertex3f(0.5f, -0.5f, 0.5f);
+			GL11.glVertex3f(0.5f, -0.5f, -0.5f);
+		}
+		if(renderXDown){
+			GL11.glColor3f(0.0f, 0.0f, 0.5f);
+			GL11.glVertex3f(-0.5f, 0.5f, 0.5f);
+			GL11.glVertex3f(-0.5f, 0.5f, -0.5f);
+			GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
+			GL11.glVertex3f(-0.5f, -0.5f, 0.5f);
+		}
+		if(renderYUp){
+			GL11.glColor3f(0.5f, 0.5f, 0.0f);
+			GL11.glVertex3f(0.5f, 0.5f, -0.5f);
+			GL11.glVertex3f(-0.5f, 0.5f, -0.5f);
+			GL11.glVertex3f(-0.5f, 0.5f, 0.5f);
+			GL11.glVertex3f(0.5f, 0.5f, 0.5f);
+		}
+		if(renderYDown){
+			GL11.glColor3f(0.5f, 0.5f, 0.0f);
+			GL11.glVertex3f(0.5f, -0.5f, 0.5f);
+			GL11.glVertex3f(-0.5f, -0.5f, 0.5f);
+			GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
+			GL11.glVertex3f(0.5f, -0.5f, -0.5f);
+		}
+		if(renderZUp){
+			GL11.glColor3f(0.5f, 0.0f, 0.0f);
+			GL11.glVertex3f(0.5f, 0.5f, 0.5f);
+			GL11.glVertex3f(-0.5f, 0.5f, 0.5f);
+			GL11.glVertex3f(-0.5f, -0.5f, 0.5f);
+			GL11.glVertex3f(0.5f, -0.5f, 0.5f);
+		}
+		if(renderZDown){
+			GL11.glColor3f(0.5f, 0.5f, 0.0f);
+			GL11.glVertex3f(0.5f, -0.5f, -0.5f);
+			GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
+			GL11.glVertex3f(-0.5f, 0.5f, -0.5f);
+			GL11.glVertex3f(0.5f, 0.5f, -0.5f);
+		}
 		GL11.glEnd();
-		GL11.glPopMatrix();
 	}
 }
--- WraithEngine/src/wraith/library/LWJGL/RenderableObject3D.java
+++ WraithEngine/src/wraith/library/LWJGL/RenderableObject3D.java
@@ -1,5 +1,13 @@
 package wraith.library.LWJGL;
 
+import org.lwjgl.opengl.GL11;
+
 public abstract class RenderableObject3D extends Object3D{
-	public abstract void renderObject();
+	public void renderObject(){
+		GL11.glPushMatrix();
+		translateMatrix();
+		draw();
+		GL11.glPopMatrix();
+	}
+	protected abstract void draw();
 }
