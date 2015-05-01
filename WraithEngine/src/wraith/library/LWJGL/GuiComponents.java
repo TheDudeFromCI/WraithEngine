e168647780ce42fc994502cdad308f6922b4507f
TheDudeFromCI <linktheblademaster@yahoo.com>
2015-05-01 08:42:01 -0700

    Fixed aspect issues.

--- WraithEngine/src/wraith/library/LWJGL/GuiComponents.java
+++ WraithEngine/src/wraith/library/LWJGL/GuiComponents.java
@@ -5,24 +5,25 @@ import java.util.ArrayList;
 import static org.lwjgl.opengl.GL11.*;
 
 public class GuiComponents{
-	public float scale = 1;
+	public float scaleX = 1;
+	public float scaleY = 1;
 	private GuiImage img;
 	private final Dimension screenSize;
 	private final ArrayList<GuiImage> images = new ArrayList();
 	public void render(){
-		MatrixUtils.setupImageOrtho(screenSize.width*scale, screenSize.height*scale, -1, 1);
+		MatrixUtils.setupImageOrtho(screenSize.width*scaleX, screenSize.height*scaleY, -1, 1);
 		for(int i = 0; i<images.size(); i++){
 			img=images.get(i);
 			img.texture.bind();
 			glBegin(GL_QUADS);
 			glTexCoord2f(0, 0);
-			glVertex3f(screenSize.width*img.x, screenSize.height*img.y, img.z);
+			glVertex3f(screenSize.width*img.x*scaleX, screenSize.height*img.y*scaleY, img.z);
 			glTexCoord2f(0, 1);
-			glVertex3f(screenSize.width*(img.x+img.w), screenSize.height*img.y, img.z);
+			glVertex3f(screenSize.width*(img.x+img.w)*scaleX, screenSize.height*img.y*scaleY, img.z);
 			glTexCoord2f(1, 1);
-			glVertex3f(screenSize.width*(img.x+img.w), screenSize.height*(img.y+img.h), img.z);
+			glVertex3f(screenSize.width*(img.x+img.w)*scaleX, screenSize.height*(img.y+img.h)*scaleY, img.z);
 			glTexCoord2f(1, 0);
-			glVertex3f(screenSize.width*img.x, screenSize.height*(img.y+img.h), img.z);
+			glVertex3f(screenSize.width*img.x*scaleX, screenSize.height*(img.y+img.h)*scaleY, img.z);
 			glEnd();
 		}
 	}
