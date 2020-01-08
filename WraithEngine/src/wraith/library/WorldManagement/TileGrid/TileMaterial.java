4faee8d77007e8c1e867c0f40b82b3d3f039e050
TheDudeFromCI <thedudefromci@gmail.com>
2020-01-07 23:28:45 -0700

    Removed unnessicary files.

--- WraithEngine/src/wraith/library/WorldManagement/TileGrid/TileMaterial.java
+++
@@ -1,7 +0,0 @@
-package wraith.library.WorldManagement.TileGrid;
-
-import java.awt.image.BufferedImage;
-
-public interface TileMaterial{
-	public BufferedImage getImage();
-}
--- WraithEngine/src/wraith/library/WorldManagement/TileGrid/WorldPopulator.java
+++
@@ -1,5 +0,0 @@
-package wraith.library.WorldManagement.TileGrid;
-
-public interface WorldPopulator{
-	public void generate(Tile[][][] tiles);
-}
