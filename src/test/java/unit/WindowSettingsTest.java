b0873f8ec98ad32f584376ce51d47e8257967412
TheDudeFromCI <thedudefromci@gmail.com>
2020-01-08 19:49:12 -0700

    Added tests for Window Settings.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

---
+++ src/test/java/unit/WindowSettingsTest.java
@@ -0,0 +1,49 @@
+package unit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import net.whg.we.window.WindowSettings;
+
+public class WindowSettingsTest
+{
+    @Test
+    public void properties_getAndSet()
+    {
+        WindowSettings settings = new WindowSettings();
+
+        settings.setTitle("New Title");
+        assertEquals("New Title", settings.getTitle());
+
+        settings.setWidth(100);
+        assertEquals(100, settings.getWidth());
+
+        settings.setHeight(120);
+        assertEquals(120, settings.getHeight());
+
+        settings.setFullscreen(true);
+        assertEquals(true, settings.isFullscreen());
+
+        settings.setVsync(true);
+        assertEquals(true, settings.isVsync());
+
+        settings.setResizable(true);
+        assertEquals(true, settings.isResizable());
+
+        settings.setSamples(6);
+        assertEquals(6, settings.getSamples());
+    }
+
+    @Test
+    public void properties_getDefaults()
+    {
+        WindowSettings settings = new WindowSettings();
+
+        assertEquals("Untitled", settings.getTitle());
+        assertEquals(800, settings.getWidth());
+        assertEquals(600, settings.getHeight());
+        assertEquals(false, settings.isFullscreen());
+        assertEquals(false, settings.isVsync());
+        assertEquals(false, settings.isResizable());
+        assertEquals(1, settings.getSamples());
+    }
+}
