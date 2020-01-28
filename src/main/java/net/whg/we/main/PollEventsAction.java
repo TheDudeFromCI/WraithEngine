6e04b89863f68af4376edb247bee0980b88b88d6
TheDudeFromCI <thedudefromci@gmail.com>
2020-01-28 11:28:24 -0700

    Added poll events action.

---
+++ src/main/java/net/whg/we/main/PollEventsAction.java
@@ -0,0 +1,36 @@
+package net.whg.we.main;
+
+import net.whg.we.window.IWindow;
+
+/**
+ * The poll events action is used to trigger a window to poll events and swap
+ * the render buffer, which needs to be done at the end of every frame in order
+ * to render the game to the screen.
+ */
+public class PollEventsAction implements ILoopAction
+{
+    private final IWindow window;
+
+    /**
+     * Creates a new poll-events action.
+     * 
+     * @param window
+     *     - The window to poll the events for each frame.
+     */
+    public PollEventsAction(IWindow window)
+    {
+        this.window = window;
+    }
+
+    @Override
+    public void run()
+    {
+        window.pollEvents();
+    }
+
+    @Override
+    public int getPriority()
+    {
+        return PipelineConstants.POLL_WINDOW_EVENTS;
+    }
+}
