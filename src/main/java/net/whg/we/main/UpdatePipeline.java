abedf375b35d8d0802301e6d76124890098cb171
TheDudeFromCI <thedudefromci@gmail.com>
2020-01-29 13:19:36 -0700

    The update pipeline now holds a timer reference.

--- src/main/java/net/whg/we/main/UpdatePipeline.java
+++ src/main/java/net/whg/we/main/UpdatePipeline.java
@@ -3,15 +3,31 @@ package net.whg.we.main;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * The update pipeline is triggered each frame to prepare a scene to be
+ * rendered, or run logic which needs to be executed every frame.
+ */
 public class UpdatePipeline implements IPipelineAction
 {
     private final List<IUpdateable> objects = new CopyOnWriteArrayList<>();
+    private final Timer timer;
+
+    /**
+     * Creates a new update pipeline object.
+     * 
+     * @param timer
+     *     - The timer associated with this update pipeline.
+     */
+    public UpdatePipeline(Timer timer)
+    {
+        this.timer = timer;
+    }
 
     @Override
     public void run()
     {
         for (IUpdateable obj : objects)
-            obj.update();
+            obj.update(timer);
     }
 
     @Override
--- src/test/java/unit/UpdatePipelineActionTest.java
+++ src/test/java/unit/UpdatePipelineActionTest.java
@@ -1,11 +1,13 @@
 package unit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import org.junit.Test;
 import net.whg.we.main.AbstractBehavior;
 import net.whg.we.main.IUpdateable;
 import net.whg.we.main.PipelineConstants;
+import net.whg.we.main.Timer;
 import net.whg.we.main.UpdatePipeline;
 
 public class UpdatePipelineActionTest
@@ -13,13 +15,14 @@ public class UpdatePipelineActionTest
     @Test
     public void ensurePipelinePriority()
     {
-        assertEquals(PipelineConstants.FRAME_UPDATES, new UpdatePipeline().getPriority());
+        assertEquals(PipelineConstants.FRAME_UPDATES, new UpdatePipeline(mock(Timer.class)).getPriority());
     }
 
     @Test
     public void updateBehaviors()
     {
-        UpdatePipeline action = new UpdatePipeline();
+        Timer timer = mock(Timer.class);
+        UpdatePipeline action = new UpdatePipeline(timer);
         action.enableBehavior(mock(AbstractBehavior.class)); // To make sure no casting issues occur
 
         UpdatableAction behavior = new UpdatableAction();
@@ -28,6 +31,7 @@ public class UpdatePipelineActionTest
         action.enableBehavior(behavior);
         action.run();
         assertEquals(1, behavior.calls);
+        assertTrue(timer == behavior.timer);
 
         action.disableBehavior(behavior);
         action.run();
@@ -36,11 +40,13 @@ public class UpdatePipelineActionTest
 
     class UpdatableAction extends AbstractBehavior implements IUpdateable
     {
+        Timer timer;
         int calls = 0;
 
         @Override
-        public void update()
+        public void update(Timer timer)
         {
+            this.timer = timer;
             calls++;
         }
     }
